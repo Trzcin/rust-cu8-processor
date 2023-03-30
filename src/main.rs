5d368cedf5206bb2ecc7231224411c91020da4cd
Trzcin <trzccin@gmail.com>
2023-03-30 14:31:59 +0200

    cu8reader and printer

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,73 @@
-use std::{fs::File, io::{BufReader, Read, ErrorKind}, time::Instant};
+use std::{fs::File, io::{BufReader, Read, ErrorKind}};
+use num_complex::Complex;
 
-const BUFFER_SIZE: usize = 512;
-fn main() {
-    let reader = CU8Reader::from_file("/home/trzcinkde/Documents/Programming/rust/fm-audio-proccess/data/fm1_99M726_1M92.cu8");
-    reader.run();
+trait ComplexSocket {
+    fn process(&self, data: Vec<Complex<f32>>);
+}
+trait RealSocket {
+    fn process(&self, data: Vec<f32>);
 }
 
-struct CU8Reader(&'static str);
-impl CU8Reader {
-    fn from_file(file_name: &'static str) -> CU8Reader {
-        CU8Reader(file_name)
+const BUFFER_SIZE: usize = 512;
+struct CU8Reader<'a> {
+    file_name: &'static str,
+    connected: Option<&'a dyn ComplexSocket>
+}
+impl<'a> CU8Reader<'a> {
+    fn from_file(path: &'static str) -> CU8Reader {
+        CU8Reader { file_name: path, connected: None }
     }
 
-    fn run(&self) {
-        let file = File::open(self.0).expect("could not open cu8 file");
-        let mut buf_reader = BufReader::new(file);
-        let mut byte_count = 0;
+    fn connect(&mut self, socket: &'a dyn ComplexSocket) {
+        self.connected = Some(socket);
+    }
+
+    fn read(&self) {
+        let file = File::open(self.file_name).expect("could not open cu8 file");
+        let mut reader = BufReader::new(file);
         let mut buffer = [0; BUFFER_SIZE];
-        let t1 = Instant::now();
         loop {
-            match buf_reader.read(&mut buffer) {
-                Ok(0) => break,
-                Ok(n) => {
-                    byte_count += n;
-                }
+            match reader.read(&mut buffer) {
                 Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
                 Err(e) => panic!("{:?}", e),
+                Ok(0) => break,
+                Ok(_) => {
+                    let output = buffer.chunks(2).map(|pair| {
+                        Complex::new(
+                            (pair[0] as f32 - 127.5) / 127.5,
+                            (pair[1] as f32 - 127.5) / 127.5
+                        )
+                    }).collect();
+
+                    match self.connected {
+                        None => continue,
+                        Some(socket) => socket.process(output)
+                    }
+                }
             }
         }
-        let duration = t1.elapsed().as_secs_f64();
-        let speed = (byte_count as f64) / 1_048_576f64 / duration;
-        println!("Found: {} bytes in {:.2} seconds. Speed: {:.1} MB/s", byte_count, duration, speed);
     }
+}
+
+struct Printer;
+impl ComplexSocket for Printer {
+    fn process(&self, data: Vec<Complex<f32>>) {
+        for num in data {
+            println!("{}", num);
+        }
+    }
+}
+impl RealSocket for Printer {
+    fn process(&self, data: Vec<f32>) {
+        for num in data {
+            println!("{}", num);
+        }
+    }
+}
+
+fn main() {
+    let mut reader = CU8Reader::from_file("some/path/file.cu8");
+    let printer = Printer;
+    reader.connect(&printer);
+    reader.read();
 }
