93eda256e0bf639bd14b084d3b6ec1b4303be953
Trzcin <trzccin@gmail.com>
2023-03-30 22:45:07 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,66 @@
-use std::{fs::File, io::{BufReader, Read, ErrorKind}};
 use num_complex::Complex;
+#[allow(dead_code)]
 
-trait ComplexSocket {
-    fn process(&self, data: Vec<Complex<f32>>);
+enum FmData {
+    ComplexData(Vec<Complex<f32>>),
+    RealData(Vec<f32>)
 }
-trait RealSocket {
-    fn process(&self, data: Vec<f32>);
+
+trait ProcessingNode {
+    fn process(&self, data: FmData) -> FmData;
 }
 
-const BUFFER_SIZE: usize = 512;
-struct CU8Reader<'a> {
-    file_name: &'static str,
-    connected: Option<&'a dyn ComplexSocket>
-}
-impl<'a> CU8Reader<'a> {
-    fn from_file(path: &'static str) -> CU8Reader {
-        CU8Reader { file_name: path, connected: None }
-    }
-
-    fn connect(&mut self, socket: &'a dyn ComplexSocket) {
-        self.connected = Some(socket);
-    }
-
-    fn read(&self) {
-        let file = File::open(self.file_name).expect("could not open cu8 file");
-        let mut reader = BufReader::new(file);
-        let mut buffer = [0; BUFFER_SIZE];
-        loop {
-            match reader.read(&mut buffer) {
-                Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
-                Err(e) => panic!("{:?}", e),
-                Ok(0) => break,
-                Ok(_) => {
-                    let output = buffer.chunks(2).map(|pair| {
-                        Complex::new(
-                            (pair[0] as f32 - 127.5) / 127.5,
-                            (pair[1] as f32 - 127.5) / 127.5
-                        )
-                    }).collect();
-
-                    match self.connected {
-                        None => continue,
-                        Some(socket) => socket.process(output)
-                    }
-                }
-            }
+struct Decimator(u8);
+impl ProcessingNode for Decimator {
+    fn process(&self, data: FmData) -> FmData {
+        match data {
+            FmData::ComplexData(data) => todo!(),
+            FmData::RealData(data) => todo!()
         }
     }
 }
 
-struct Printer;
-impl ComplexSocket for Printer {
-    fn process(&self, data: Vec<Complex<f32>>) {
-        for num in data {
-            println!("{}", num);
+struct FmShifter(f32);
+impl ProcessingNode for FmShifter {
+    fn process(&self, data: FmData) -> FmData {
+        match data {
+            FmData::RealData(_) => panic!("FmShifter expected complex data, received real data"),
+            FmData::ComplexData(data) => todo!()
         }
     }
 }
-impl RealSocket for Printer {
-    fn process(&self, data: Vec<f32>) {
-        for num in data {
-            println!("{}", num);
+
+struct FmDemodulator;
+impl ProcessingNode for FmDemodulator {
+    fn process(&self, data: FmData) -> FmData {
+        match data {
+            FmData::RealData(_) => panic!("FmDemodulator expected complex data, received real data"),
+            FmData::ComplexData(data) => todo!()
         }
+    }
+}
+
+#[allow(dead_code)]
+struct FmProcessor {
+    input_file: &'static str,
+    output_file: &'static str,
+    nodes: Vec<Box<dyn ProcessingNode>>
+}
+#[allow(dead_code)]
+impl FmProcessor {
+    fn new(input: &'static str, output: &'static str) -> FmProcessor {
+        FmProcessor { input_file: input, output_file: output, nodes: vec![] }
+    }
+
+    fn add_node(&mut self, node: Box<dyn ProcessingNode>) {
+        self.nodes.push(node);
+    }
+
+    fn start() {
+        todo!();
     }
 }
 
 fn main() {
-    let mut reader = CU8Reader::from_file("some/path/file.cu8");
-    let printer = Printer;
-    reader.connect(&printer);
-    reader.read();
+
 }
